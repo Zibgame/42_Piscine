d8ae4f7e6e306986b08bc89d47920b59a001a03e
Zibrian Cadinot <zcadinot@c1r3p7.42lehavre.fr>
2025-07-08 10:57:55 +0200

    salut

--- shell01/ex04/MAC.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ifconfig | grep ether | awk '{print $2}'
-
-: '
-ifconfig affiche les interfaces reseau en gros.
-grep prend juste les lignes avec "ether" dedans.
-awk decoupe la ligne en colonnes et affiche pour chaque ligne la deuxideme colonne.
-'
