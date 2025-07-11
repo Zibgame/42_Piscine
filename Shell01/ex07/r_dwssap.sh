15e6ce1fd10bd26c24736e6db1801a9574adef89
Zibgame <fanizicy@gmail.com>
2025-07-11 22:32:44 +0200

    42

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cat /etc/passwd 
+cat /etc/passwd \
+| grep -v '^#' \
+| sed -n '2~2p' \
+| cut -d: -f1 \
+| rev \
+| sort -r \
+| awk -v line1="$FT_LINE1" -v line2="$FT_LINE2" '$0 >= line1 && $0 <= line2' \
+| paste -sd ', ' - \
+| sed 's/$/./'
+
