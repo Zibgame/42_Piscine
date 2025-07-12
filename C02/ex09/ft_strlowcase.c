98c71bb1f54f24d0a2a220fb03ef2347c09769e0
Zibgame <fanizicy@gmail.com>
2025-07-12 22:05:27 +0200

    42

--- C02/ex09/ft_strlowcase.c
+++ C02/ex09/ft_strlowcase.c
@@ -14,36 +14,37 @@ char	*ft_strcapitalize(char *str)
 {
 	int	i;
 	int	bool;
-	
+
 	i = 0;
 	bool = 1;
 	while (str[i] != '\0')
 	{
-		if (bool)
-			str[i] = str[i] - 32;
-		if (str[i] == 32)
-			bool = 1;
-		else
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if (bool && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		if ((str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= '0' && str[i] <= '9'))
 			bool = 0;
+		else
+			bool = 1;
 		i++;
 	}
-	return str;
+	return (str);
 }
-
-
-
+/*
 #include <unistd.h>
-int main()
-{
-	int i;
-	char str[] = "salut sa vas";
+int main() {
+  int i;
+  char str[] = "hi, how are you? 42WORDS forty-two; fifty+and+one";
 
-	ft_strcapitalize(str);
-	i = 0;
-	while(str[i] != '\0')
-	{
-		write(1,&str[i],1);
-		i++;
-	}
-	return 0;
+  ft_strcapitalize(str);
+  i = 0;
+  while (str[i] != '\0') {
+    write(1, &str[i], 1);
+    i++;
+  }
+  return 0;
 }
+*/
