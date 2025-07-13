439d5dc92a55a635d6f9daef6ba9aba4327a2e04
Zibgame <fanizicy@gmail.com>
2025-07-14 00:43:14 +0200

    42

--- Shell01/ex01/print_groups.sh
+++ Shell01/ex01/print_groups.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-groups $FT_USER | tr ' ' ',' | tr -d '\n'
+id -Gn $FT_USER | tr ' ' ',' | tr -d '\n'
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-cat /etc/passwd \
-| grep -v '^#' \
-| sed -n '2~2p' \
-| cut -d: -f1 \
-| rev \
-| sort -r \
-| awk -v line1="$FT_LINE1" -v line2="$FT_LINE2" '$0 >= line1 && $0 <= line2' \
-| paste -sd ', ' - \
-| sed 's/$/./'
-
+cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | tr '\n' , | sed 's/,$/./g' | sed 's/,/, /g' | tr -d '\n'
