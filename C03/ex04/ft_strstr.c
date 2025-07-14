424c52801c6f22f428e78aeefffe407727c67d15
Zibgame <fanizicy@gmail.com>
2025-07-14 02:01:08 +0200

    42

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,21 @@
+char    *ft_strstr(char *str, char *to_find)
+{
+    int i;
+    int j;
+
+    if (to_find[0] == '\0') 
+        return str;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        j = 0;
+        while (str[i + j] != '\0' && to_find[j] != '\0' && str[i + j] == to_find[j])
+            j++;
+        if (to_find[j] == '\0')
+            return &str[i];
+        i++;
+    }
+    return NULL;
+}
+
