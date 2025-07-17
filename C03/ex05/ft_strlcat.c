37deb71aecb7eeb62812a37fecbd3200fc2bc53f
Zibgame <fanizicy@gmail.com>
2025-07-17 20:59:21 +0200

    42

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -12,28 +12,20 @@
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	src_len;
-	unsigned int	dest_len;
+	unsigned int	i = 0;
+	unsigned int	j = 0;
+	unsigned int	src_len = 0;
+	unsigned int	dest_len = 0;
 
-	i = 0;
-	j = 0;
-	dest_len = 0;
-	src_len = 0;
-	while (dest[dest_len] != '\0')
-		dest_len++;
 	while (src[src_len] != '\0')
 		src_len++;
-	if (size <= dest_len)
+	while (dest[dest_len] != '\0' && dest_len < size)
+		dest_len++;
+	if (dest_len >= size)
 		return (size + src_len);
 	i = dest_len;
-	while (i < size - 1 && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	while (src[j] != '\0' && i < size - 1)
+		dest[i++] = src[j++];
 	dest[i] = '\0';
 	return (dest_len + src_len);
 }
