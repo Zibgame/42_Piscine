a65aa0b87157cbdeb3de9044f858dbf45b3caa52
Zibgame <fanizicy@gmail.com>
2025-07-21 22:56:31 +0200

    42

---
+++ C08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 22:07:59 by zcadinot          #+#    #+#             */
+/*   Updated: 2025/07/21 22:10:22 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCKSTR_H
+#define FT_STOCKSTR_H
+
+typedef struct s_stock_str
+{
+    int size;
+    char *str;
+    char *copy;
+}   t_stock_str;
+
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 22:05:44 by zcadinot          #+#    #+#             */
+/*   Updated: 2025/07/21 22:21:21 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+
+}
