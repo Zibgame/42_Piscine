69f3b8a312fdf780f8876ef54e466d3a0d3aa4b1
Zibgame <fanizicy@gmail.com>
2025-07-21 22:04:33 +0200

    42

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,18 +6,22 @@
 /*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 14:02:12 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/07/21 16:58:42 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/07/21 21:38:07 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-typedef t_bool int
+
+# include <unistd.h>
+
 # define TRUE 1
 # define FALSE 0
+# define EVEN(nb) ((nb) % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
-# define OOD_MSG "I have an odd number of arguments.\n"
-# define EVEN(nb) (nb % 2)
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
 
+typedef int	t_bool;
 
 #endif
